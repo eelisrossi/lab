f33e1cf904c314d1fb99751cd9c87a1be80a6d09
Eelis Rossi <eelis.rossi@gmail.com>
2025-07-30 12:43:56 +0300

    feat: two node dns setup finished and working

--- homelab/bind9/terraform/lxc.tf
+++ homelab/bind9/terraform/lxc.tf
@@ -8,7 +8,7 @@ variable "ct_ssh_keys" {
   sensitive = true
 }
 
-resource "proxmox_lxc" "bind9" {
+resource "proxmox_lxc" "ns1" {
   target_node  = "srv-mox"
   hostname     = "hlct-ns1"
   vmid         = 601
@@ -34,6 +34,8 @@ resource "proxmox_lxc" "bind9" {
     size    = "16G"
   }
 
+  nameserver = "1.1.1.1"
+
   network {
     name   = "eth0"
     bridge = "VLAN10"
@@ -41,10 +43,42 @@ resource "proxmox_lxc" "bind9" {
     gw     = "192.168.10.1"
   }
 
+  tags = "almalinux;iac;lxc;dns"
+}
 
-  tags = "almalinux;iac;lxc"
+resource "proxmox_lxc" "ns2" {
+  target_node  = "srv-prox"
+  hostname     = "hlct-ns2"
+  vmid         = 602
+  ostemplate   = "local:vztmpl/almalinux-9-amd64-cloud-20250725-rootfs.tar.xz"
+  password     = var.ct_root_password
+  unprivileged = true
 
-  provisioner "local-exec" {
-    command = "ANSIBLE_CONFIG=../ansible/ansible.cfg ansible-playbook -i ../ansible/hosts.yml ../ansible/install-ssh.yml"
+  cores    = 2
+  cpulimit = 2
+  memory   = 2048
+
+  start  = true
+  onboot = true
+
+  ssh_public_keys = var.ct_ssh_keys
+
+  features {
+    nesting = true
   }
+
+  rootfs {
+    storage = "local-lvm"
+    size    = "16G"
+  }
+
+  network {
+    name   = "eth0"
+    bridge = "VLAN10"
+    ip     = "192.168.10.6/24"
+    gw     = "192.168.10.1"
+  }
+
+
+  tags = "almalinux;iac;lxc;dns"
 }
