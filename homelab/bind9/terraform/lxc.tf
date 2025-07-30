48935b8abe362cda19a56d4e41855bec68a25eb3
Eelis Rossi <eelis.rossi@gmail.com>
2025-07-30 08:03:37 +0300

    feat: automated dns-server setup

--- homelab/bind9/terraform/lxc.tf
+++ homelab/bind9/terraform/lxc.tf
@@ -8,36 +8,39 @@ variable "ct_ssh_keys" {
   sensitive = true
 }
 
-resource "proxmox_lxc" "glance" {
+resource "proxmox_lxc" "bind9" {
   target_node  = "srv-mox"
-  hostname     = "hlct-glance"
-  vmid         = 600
+  hostname     = "hlct-ns1"
+  vmid         = 601
   ostemplate   = "local:vztmpl/almalinux-9-amd64-cloud-20250725-rootfs.tar.xz"
   password     = var.ct_root_password
   unprivileged = true
 
-  cores    = 1
-  cpulimit = 1
-  memory   = 1024
+  cores    = 2
+  cpulimit = 2
+  memory   = 2048
 
   start  = true
   onboot = true
 
   ssh_public_keys = var.ct_ssh_keys
 
-  rootfs {
-    storage = "local-lvm"
-    size    = "8G"
+  features {
+    nesting = true
   }
 
-  nameserver = "192.168.10.20"
+  rootfs {
+    storage = "local-lvm"
+    size    = "16G"
+  }
 
   network {
     name   = "eth0"
     bridge = "VLAN10"
-    ip     = "192.168.10.50/24"
+    ip     = "192.168.10.5/24"
     gw     = "192.168.10.1"
   }
+
 
   tags = "almalinux;iac;lxc"
 
